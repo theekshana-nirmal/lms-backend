aba7d1b87c80191bcaaf91a6918f847f79ccad95
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-24 00:02:29 +0530

    Added missing Repository annotation to  UserRepository

--- src/main/java/lk/uwu/lms_backend/repositories/UserRepository.java
+++ src/main/java/lk/uwu/lms_backend/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package lk.uwu.lms_backend.repositories;
 
 import lk.uwu.lms_backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
