a35baeb40db4c1efd3bcee2f57c6e51333385b89
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-23 23:44:09 +0530

    Created User entity

---
+++ src/main/java/lk/uwu/lms_backend/entities/User.java
@@ -0,0 +1,50 @@
+package lk.uwu.lms_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "hashed_password")
+    private String hashed_password;
+
+    @Column(name = "profile_photo_url")
+    private String profile_photo_url = "https://placehold.co/100x100";
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, updatable = false)
+    private Role role;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
