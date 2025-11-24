042ec056f33d9c3f997fe7ae6ba2baf7e58870c9
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-24 14:46:12 +0530

    add Component annotation to CustomUserDetailsService for Spring context

--- src/main/java/lk/uwu/lms_backend/security/CustomUserDetailsService.java
+++ src/main/java/lk/uwu/lms_backend/security/CustomUserDetailsService.java
@@ -14,10 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
 import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
