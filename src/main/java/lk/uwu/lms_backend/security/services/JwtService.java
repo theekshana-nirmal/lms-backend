6fabec4a2a0b5a0ce04120f326eff59d5204e750
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-24 23:36:15 +0530

    add JwtService for handling JWT token generation and validation

---
+++ src/main/java/lk/uwu/lms_backend/security/services/JwtService.java
@@ -0,0 +1,58 @@
+package lk.uwu.lms_backend.security.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lk.uwu.lms_backend.entities.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${JWT_SECRET}")
+    private String secret_key;
+
+    // Get Signature Key
+    private SecretKey getSignInKey(){
+        //Change secret key text into a real security key (make a SecretKey object to sign JWT tokens.)
+        byte[] decode = Decoders.BASE64.decode(secret_key);
+        return Keys.hmacShaKeyFor(decode);
+    }
+
+    // Extract all claims from token
+    private Claims extractAllClaims(String token){
+        // Takes a JWT token, checks if it is valid, then reads all information inside the token (claims)
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    // Extract specific claim from token
+    private <T> T extractClaims(String token, Function<Claims, T> claimResolver){
+        Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    // Extract Username
+    private String extractUserEmail(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    // Generate JWT Tokens
+    public String generateToken(User user){
+        return null;
+    }
+
+    // Validate token
+
+    // Extract expiration date
+
+    // Check if token expired
+}
