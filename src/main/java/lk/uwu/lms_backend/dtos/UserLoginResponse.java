833f164b4415c8900c302ca3c6b20ad31b94e94b
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-01 14:27:35 +0530

    add UserLoginResponse DTO for handling user login responses

---
+++ src/main/java/lk/uwu/lms_backend/dtos/UserLoginResponse.java
@@ -0,0 +1,16 @@
+package lk.uwu.lms_backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserLoginResponse {
+    private String accessToken;
+    private String refreshToken;
+    private long expiresIn;
+}
