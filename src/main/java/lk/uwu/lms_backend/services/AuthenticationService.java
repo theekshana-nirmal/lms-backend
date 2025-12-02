fed95d6acc4c7a542799bd0d867dac851f0b2911
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-02 22:04:51 +0530

    feat: implement basic authentication with HttpOnly refresh token cookie

--- src/main/java/lk/uwu/lms_backend/services/AuthenticationService.java
+++ src/main/java/lk/uwu/lms_backend/services/AuthenticationService.java
@@ -1,5 +1,7 @@
 package lk.uwu.lms_backend.services;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.uwu.lms_backend.dtos.UserAuthResponseDTO;
 import lk.uwu.lms_backend.dtos.UserLoginRequestDTO;
 import lk.uwu.lms_backend.dtos.UserRegistrationRequestDTO;
@@ -27,7 +29,7 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     // User registration
-    public UserAuthResponseDTO registerUser(UserRegistrationRequestDTO request) {
+    public UserAuthResponseDTO registerUser(UserRegistrationRequestDTO request, HttpServletResponse response) {
         // Check if user already exists
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new UserAlreadyExistsException("User with email " + request.getEmail() + " already exists");
@@ -42,11 +44,11 @@ public class AuthenticationService {
 
         // Save user to the database
         User savedUser = userRepository.save(user);
-        return authResponse(savedUser);
+        return authResponse(savedUser, response);
     }
 
     // User Login
-    public UserAuthResponseDTO loginUser(UserLoginRequestDTO request) throws UserCredentialsInvalidException {
+    public UserAuthResponseDTO loginUser(UserLoginRequestDTO request, HttpServletResponse response) throws UserCredentialsInvalidException {
         try {
             // Authenticate user credentials
             authenticationManager.authenticate(
@@ -60,11 +62,11 @@ public class AuthenticationService {
         // Retrieve user from the database
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UserNotFoundException("User with email " + request.getEmail() + " not found"));
-        return authResponse(user);
+        return authResponse(user, response);
     }
 
     // Make authentication response with JWT tokens
-    private UserAuthResponseDTO authResponse(User user) {
+    private UserAuthResponseDTO authResponse(User user, HttpServletResponse response) {
         // Convert to UserDetails
         CustomUserDetails userDetails = new CustomUserDetails(user);
 
@@ -72,10 +74,17 @@ public class AuthenticationService {
         String accessToken = jwtService.generateAccessToken(new HashMap<>(), userDetails);
         String refreshToken = jwtService.generateRefreshToken(new HashMap<>(), userDetails);
 
+        // Set Refresh Token as a HttpOnly cookie
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setPath("/");
+        // refreshTokenCookie.setSecure(true);
+        refreshTokenCookie.setMaxAge((int) TimeUnit.DAYS.toSeconds(30));
+        response.addCookie(refreshTokenCookie);
+
         return new UserAuthResponseDTO(
                 user.getEmail(),
                 accessToken,
-                refreshToken,
                 user.getRole().name(),
                 TimeUnit.MINUTES.toMillis(30));
     }
