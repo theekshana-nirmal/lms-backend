360e0ee04cdf524f2a5d4fbd62a677618e9a2f16
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-12 14:35:16 +0530

    feat: enhance UserDetailsResponseDTO with additional fields

--- src/main/java/lk/uwu/lms_backend/dtos/UserDetailsResponseDTO.java
+++ src/main/java/lk/uwu/lms_backend/dtos/UserDetailsResponseDTO.java
@@ -1,13 +1,24 @@
 package lk.uwu.lms_backend.dtos;
 
+import lk.uwu.lms_backend.entities.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDetailsResponseDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePhotoUrl;
+    private String role;
+    private String createdAt;
+    private String updatedAt;
 }
