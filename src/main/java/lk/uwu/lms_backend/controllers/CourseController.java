93f7cc7a687b9605c181d56567a5ec264848fd21
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-12 21:12:38 +0530

    feat: add createCourse endpoint to CourseController for course creation

--- src/main/java/lk/uwu/lms_backend/controllers/CourseController.java
+++ src/main/java/lk/uwu/lms_backend/controllers/CourseController.java
@@ -1,14 +1,16 @@
 package lk.uwu.lms_backend.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lk.uwu.lms_backend.dtos.CourseRequestDTO;
 import lk.uwu.lms_backend.dtos.CourseResponseDTO;
 import lk.uwu.lms_backend.dtos.ResponseDTO;
 import lk.uwu.lms_backend.services.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +24,15 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<ResponseDTO<List<CourseResponseDTO>>> getAllCourses() {
         return ResponseEntity.status(HttpStatus.OK).body(courseService.getAllCourses());
+    }
+
+    // Create a Course
+    @PreAuthorize("hasRole('TEACHER')")
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO<CourseResponseDTO>> createCourse(
+            @Validated @RequestBody CourseRequestDTO request,
+            HttpServletRequest httpServletRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.createCourse(request));
     }
 }
