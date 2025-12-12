eaa7df51d5869fc65c510607402460d153985347
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-12 13:57:16 +0530

    feat: add UserDetailsResponseDTO for user details response handling

---
+++ src/main/java/lk/uwu/lms_backend/dtos/UserDetailsResponseDTO.java
@@ -0,0 +1,13 @@
+package lk.uwu.lms_backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDetailsResponseDTO {
+}
