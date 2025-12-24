ed9569a72a95219ff5a043e2552e4dd5078cc99c
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-24 23:23:09 +0530

    feat: update AuthenticationController and JwtAuthenticationFilter for improved token handling

--- src/main/java/lk/uwu/lms_backend/config/JwtAuthenticationFilter.java
+++ src/main/java/lk/uwu/lms_backend/config/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.uwu.lms_backend.services.CustomUserDetailsService;
 import lk.uwu.lms_backend.services.JwtService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -27,8 +28,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain)
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         // Step 1: Extract JWT from Authorization header
